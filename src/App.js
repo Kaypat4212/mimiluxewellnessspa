fc431d79606c66844c133cb6075f25e35572bc42
Patrick ezike (Kay) <109922476+Kaypat4212@users.noreply.github.com>
2024-08-26 17:29:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,65 @@
 import './App.css';
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useRef, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/home';
 import About from './components/about';
 import Booking from './components/booking';
 import Footer from './components/footer';
 import Services from './components/services';
-import './components/styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-import logo from './components/images/logo.jpg'
-import './components/styles/style.css'
-import { Link } from 'react-router-dom';
-import './components/styles/whatsappbutton.css'
+import logo from './components/images/logo.jpg';
 import WhatsAppButton from './components/whatsappbutton'; // Import the WhatsAppButton component
-function App() {
-  return (
 
+function App() {
+  const navbarCollapseRef = useRef(null);
+
+  useEffect(() => {
+    const navbarLinks = document.querySelectorAll('.nav-link');
+    
+    navbarLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        if (navbarCollapseRef.current.classList.contains('show')) {
+          // Collapse the navbar by toggling the 'show' class off
+          const collapse = new window.bootstrap.Collapse(navbarCollapseRef.current, {
+            toggle: true
+          });
+          collapse.hide();
+        }
+      });
+    });
+
+    // Cleanup event listeners
+    return () => {
+      navbarLinks.forEach(link => {
+        link.removeEventListener('click', () => {});
+      });
+    };
+  }, []);
+
+  return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="/"><img className='logo' src={logo} alt="" /></a>
 
+        <button id="menuToggle" className='navbar-toggler' type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-
-        <div id="menuToggle" className='p-5' type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <input id="checkbox" type="checkbox" />
-          <label className="toggle" for="checkbox">
-            <div className="bar bar--top"></div>
-            <div className="bar bar--middle"></div>
-            <div className="bar bar--bottom"></div>
-          </label>
-        </div>
-
-        <div className="collapse navbar-collapse justify-content-lg-end p-5" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse justify-content-lg-end p-5" id="navbarSupportedContent" ref={navbarCollapseRef}>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
             </li>
-            <li classname="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/services">Services</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/book">Book An Appointment</Link>
             </li>
-
           </ul>
         </div>
       </nav>
@@ -61,10 +72,7 @@ function App() {
       <WhatsAppButton /> {/* Include the WhatsAppButton component */}
       <Footer />
     </Router>
-    
   );
 }
 
 export default App;
-
- 
