51e4e77c2168ed75d8e0f8c6e9f7e3f8d166edd4
patrick ezike <patrickezike66@gmail.com>
2024-08-28 01:05:08 +0100

    new form

---
+++ src/components/thankyou.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const AppointmentSuccess = () => {
+  return (
+    <div>
+      <h1>Appointment Successfully Booked</h1>
+      <p>Thank you for booking an appointment with us. We will contact you shortly!</p>
+    </div>
+  );
+};
