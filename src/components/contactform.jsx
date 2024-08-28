8e203ff1d46b5f4b1b2dc5f27325ee2d975b3f90
patrick ezike <patrickezike66@gmail.com>
2024-08-28 13:41:09 +0100

    services added to form

--- src/components/contactform.jsx
+++ src/components/contactform.jsx
@@ -17,8 +17,16 @@ function ContactForm() {
   </p>
   <p>
     <label>Service: <select name="service[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
+      <option value="DeepTissueMassage">Deep Tissue Massage</option>
+      <option value="FaceMassage">Face Massage</option>
+      <option value="ReflexologyMassage">Reflexology Massage</option>
+      <option value="ShiatsuMassage">Shiatsu Massage</option>
+      <option value="Aromatherapy Massage">Aromatherapy Massage</option>
+      <option value="SwedishMassage">Swedish Massage</option>
+      <option value="NuruMassage">Nuru Massage</option>
+      <option value="HotstoneMassage">Hot Stone Massage</option>
+      <option value="MyofascialReleaseMassage">Myofascial Release Massage</option>
+      <option value="ThaiMassage">Thai Massage</option>
     </select>
     </label>
   </p>
