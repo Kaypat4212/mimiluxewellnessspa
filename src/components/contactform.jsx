cc88198082f91b7ebfce55b58d869e019832f1fc
patrick ezike <patrickezike66@gmail.com>
2024-08-29 14:41:12 +0100

    updates..

--- src/components/contactform.jsx
+++ src/components/contactform.jsx
@@ -4,46 +4,48 @@ import './styles/contactform.css';
 function ContactForm() {
   return (
     <div className='body'>
+
       <form name="contact" className='form' method="POST" netlify>
 
-    <label>Your Name: </label> <br/>
-    <input type="text" name="name" />
+        <label>Your Name: </label> 
+        <br />
+        <input type="text" name="name" />
 
-  <br/>
+        <br />
 
-    <label>Your Email: </label> <br/>
-    <input type="email" name="email" />
-  
-  <br/>
+        <label>Your Email: </label> <br />
+        <input type="email" name="email" />
 
-    <label>Phone number:</label> <br/>
-    <input type="tel" name="phone-number" />
-  
-  <br/>
+        <br />
 
-    <label>Service: 
-    </label>
-    <select className='select' name="service">
-      <option value="Select">Select one</option>
-      <option value="DeepTissueMassage">Deep Tissue Massage</option>
-      <option value="FaceMassage">Face Massage</option>
-      <option value="ReflexologyMassage">Reflexology Massage</option>
-      <option value="ShiatsuMassage">Shiatsu Massage</option>
-      <option value="Aromatherapy Massage">Aromatherapy Massage</option>
-      <option value="SwedishMassage">Swedish Massage</option>
-      <option value="NuruMassage">Nuru Massage</option>
-      <option value="HotstoneMassage">Hot Stone Massage</option>
-      <option value="MyofascialReleaseMassage">Myofascial Release Massage</option>
-      <option value="ThaiMassage">Thai Massage</option>
-    </select>
-  
+        <label>Phone number:</label> <br />
+        <input type="tel" name="phone-number" />
 
-    <label>Message: <textarea name="message"></textarea></label>
-  <br/>
+        <br />
 
-    <button type="submit">Send</button>
-  
-</form>
+        <label>Service:
+        </label>
+        <select className='select' name="service">
+          <option value="Select">Select one</option>
+          <option value="DeepTissueMassage">Deep Tissue Massage</option>
+          <option value="FaceMassage">Face Massage</option>
+          <option value="ReflexologyMassage">Reflexology Massage</option>
+          <option value="ShiatsuMassage">Shiatsu Massage</option>
+          <option value="Aromatherapy Massage">Aromatherapy Massage</option>
+          <option value="SwedishMassage">Swedish Massage</option>
+          <option value="NuruMassage">Nuru Massage</option>
+          <option value="HotstoneMassage">Hot Stone Massage</option>
+          <option value="MyofascialReleaseMassage">Myofascial Release Massage</option>
+          <option value="ThaiMassage">Thai Massage</option>
+        </select>
+
+
+        <label>Message: <textarea name="message"></textarea></label>
+        <br />
+
+        <button type="submit">Send</button>
+
+      </form>
 
 
 
