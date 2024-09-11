803acc622c02836111ceb3da9f9d4dd78ed80aca
patrick ezike <patrickezike66@gmail.com>
2024-09-11 15:01:34 +0100

    booking page fixes

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -4,15 +4,18 @@ import ContactForm from '../components/contactform';
 
 const Booking = () => {
   return (
-    <header className='headerr'>
+   <div>
     <div className="innerheader">
     <h2>Book an Appointment</h2>
     <p>Fill out the form below to schedule your appointment with us.</p>
-    </div>
+    </div> <br />
+    <header className='headerr'>
+
      <div className="contactform">
      <ContactForm/>
      </div>
     </header>
+   </div>
 
   );
 };
